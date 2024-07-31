fe38317fc2179ceb0b178ced91d92fcb2164580f
Praveen Kumar M <www.pkrofficial@gmail.com>
2024-07-31 10:13:24 +0530

    Create pro.py

---
+++ pro.py
@@ -0,0 +1,12 @@
+# sample.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition of 5 and 3 is:", add(5, 3))
+    print("Subtraction of 5 and 3 is:", subtract(5, 3))
+
