fa2120013cefb2a722ef654263cc533269508e01
Praveen Kumar M <www.pkrofficial@gmail.com>
2024-07-31 14:24:19 +0530

    Update and rename HelloWorld.java to ArmstrongNumber.java

---
+++ ArmstrongNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    // Function to check if a number is an Armstrong number
+    static boolean isArmstrong(int number) {
+        int originalNumber = number;
+        int n = String.valueOf(number).length(); // Number of digits
+        int result = 0;
+
+        while (originalNumber != 0) {
+            int digit = originalNumber % 10;
+            result += Math.pow(digit, n);
+            originalNumber /= 10;
+        }
+
+        return result == number;
+    }
+}
